a7b038c4654ab71e8825daf2e37d162a0baf610e
bhavyarangu333 <126983501+bhavyarangu333@users.noreply.github.com>
2024-12-19 20:50:14 -0600

    Initial commit

---
+++ src/components/Project.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import mock01 from '../assets/images/mock01.png';
+import mock02 from '../assets/images/mock02.png';
+import mock03 from '../assets/images/mock03.png';
+import mock04 from '../assets/images/mock04.png';
+import mock05 from '../assets/images/mock05.png';
+import mock06 from '../assets/images/mock06.png';
+import mock07 from '../assets/images/mock07.png';
+import mock08 from '../assets/images/mock08.png';
+import mock09 from '../assets/images/mock09.png';
+import mock10 from '../assets/images/mock10.png';
+import '../assets/styles/Project.scss';
+
+function Project() {
+    return(
+    <div className="projects-container" id="projects">
+        <h1>Personal Projects</h1>
+        <div className="projects-grid">
+            <div className="project">
+                <a href="https://github.com/bhavyarangu333/chatbot.git" target="_blank" rel="noreferrer"><img src={mock10} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/bhavyarangu333/chatbot.git" target="_blank" rel="noreferrer"><h2>Chat Mate</h2></a>
+                <p>Developed an intelligent chatbot using React, Node.js, OpenAI API, and MongoDB for seamless conversational experiences.</p>
+            </div>
+            <div className="project">
+                <a href="https://github.com/bhavyarangu333/Decentralized-voting-application-blockchain.git" target="_blank" rel="noreferrer"><img src={mock09} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/bhavyarangu333/Decentralized-voting-application-blockchain.git" target="_blank" rel="noreferrer"><h2>Decentralized Voting Application</h2></a>
+                <p>Developed a decentralized voting application using Hardhat, MongoDB, Volta faucet, and JavaScript, ensuring secure, transparent, and tamper-proof elections.</p>
+            </div>
+            <div className="project">
+                <a href="https://github.com/bhavyarangu333/FaceRecog-API.git" target="_blank" rel="noreferrer"><img src={mock06} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/bhavyarangu333/FaceRecog-API.git" target="_blank" rel="noreferrer"><h2>FaceRecog-API</h2></a>
+                <p>Collaborated on developing a Face Recognition API, enhancing authentication and security features for seamless integration into applications.</p>
+            </div>
+            <div className="project">
+                <a href="https://github.com/bhavyarangu333/Real-Time-Data-Processing-of-Split-wise-Data-using-Big-Data-Technologies-.git" target="_blank" rel="noreferrer"><img src={mock04} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/bhavyarangu333/Real-Time-Data-Processing-of-Split-wise-Data-using-Big-Data-Technologies-.git" target="_blank" rel="noreferrer"><h2>Real-Time Data Processing of Splitwise Data</h2></a>
+                <p>Developed a real-time data processing system for analyzing Splitwise data, leveraging Hadoop for storage, Spark for distributed processing, and Kafka for data streaming, enabling efficient handling of large-scale financial datasets.</p>
+            </div>
+            <div className="project">
+                <a href="https://github.com/bhavyarangu333/e-commerce-bookstore.git" target="_blank" rel="noreferrer"><img src={mock03} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/bhavyarangu333/e-commerce-bookstore.git" target="_blank" rel="noreferrer"><h2>E-Commerce-Bookstore</h2></a>
+                <p>
+                Developed an e-commerce bookstore platform using ASP.NET Core 8, .NET Core MVC, and SQL Server for seamless product management and secure transactions.</p>
+            </div>
+            <div className="project">
+                <a href="https://github.com/bhavyarangu333/Restaurant-Menu.git" target="_blank" rel="noreferrer"><img src={mock01} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/bhavyarangu333/Restaurant-Menu.git" target="_blank" rel="noreferrer"><h2>Restaurant Menu</h2></a>
+                <p>Developed a dynamic restaurant menu application featuring interactive UI and real-time updates using React Native and Expo.</p>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default Project;
---
+++ src/components/Timeline.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import '../assets/styles/Timeline.scss'
+
+function Timeline() {
+  return (
+    <div id="history">
+      <div className="items-container">
+        <h1>Career History</h1>
+        <VerticalTimeline>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
+            contentArrowStyle={{ borderRight: '7px solid  white' }}
+            date="May 2023 - May 2024"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Web Developer</h3>
+            <h4 className="vertical-timeline-element-subtitle">CSUG, FULLERTON</h4>
+            <p>
+              Full-stack Web Development, React, User Experience, Site Optimization, Performance Monitoring, Google Analytics, New Relic
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="Sep 2021 - Aug 2022"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Software Engineer</h3>
+            <h4 className="vertical-timeline-element-subtitle">INFOSYS, HYDERABAD</h4>
+            <p>
+            Full-stack Development, ASP.NET, C#, Azure, Machine Learning, CI/CD, Jenkins, GitHub CI, Docker, SQL, Oracle, Web Forms, Angular, JavaScript
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="Apr 2019 - Aug 2021"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Software Developer</h3>
+            <h4 className="vertical-timeline-element-subtitle">SS Computers Services Limited, HYDERABAD</h4>
+            <p>
+            Full-stack Development, API Integration, Performance Optimization, Cloud Services, Real-time Systems, Automation
+            </p>
+          </VerticalTimelineElement>
+        </VerticalTimeline>
+      </div>
+    </div>
+  );
+}
+
+export default Timeline;
